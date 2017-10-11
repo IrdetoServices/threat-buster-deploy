df6433c5e59bd016ef2e6fa1c40306a25ab8a834
Ben Gidley <ben@gidley.co.uk>
2017-10-11 15:33:15 +0100

    Neo4j now deploying to EBS volume

--- aws_cf_deploy.sh
+++ aws_cf_deploy.sh
@@ -6,5 +6,6 @@ cfn-create-or-update --stack-name $STACK_NAME --template-body file://cloudformat
     --region $REGION --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --parameters \
     ParameterKey=ScriptBucket,ParameterValue=$SCRIPT_BUCKET \
     ParameterKey=CertificateARN,ParameterValue=$CERTIFICATE_ARN \
-    ParameterKey=Image,ParameterValue=$REPO_URL/$REPO:latest --disable-rollback
+    ParameterKey=Image,ParameterValue=$REPO_URL/$REPO:latest \
+    ParameterKey=KeyName,ParameterValue=$KEY_NAME --disable-rollback
 
